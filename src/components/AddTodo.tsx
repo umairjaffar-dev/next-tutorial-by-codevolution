a1758fff47b4e7330b38638995f8544efb2cd52b
haseebmohsin <haseeb11634@gmail.com>
2024-07-08 12:28:36 +0500

    todo file changes...

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const AddTodo = () => {
   return (
-    <div>AddTodo</div>
-  )
-}
+    <div>
+      <h1 className="w-full text-center text-gray-900">
+        I want to add todos here!
+      </h1>
+    </div>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
