b354c31fa04fc59284dcf36e4c2a6014c86cdeff
haseebmohsin <haseeb11634@gmail.com>
2024-07-09 09:53:13 +0500

    private folder in nexxt js...

---
+++ src/app/_lib/formate__date.ts
@@ -0,0 +1,5 @@
+
+// this method returns the formatted date in local date and time formate.
+export const formattedDate = (value: string) => {
+  return value;
+};
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrivateFolder = () => {
+  return (
+    <div>This Private Folder can't access by the browser.</div>
+  )
+}
+
+export default PrivateFolder
